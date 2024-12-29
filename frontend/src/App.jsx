febfad903e3ae40c1d29c3cb21e93c1589046c2f
kapilh <146179927+kapilhadoltikar@users.noreply.github.com>
2024-12-29 13:10:44 +0530

    django and react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { BrowserRouter, Route, Navigate, Routes,  } from "react-router-dom"
+import ProtectedRoute from "./components/ProtectedRoute"
+import Home from "./pages/Home"
+import Login from "./pages/login"
+import Register from "./pages/Register"
+import NotFound from "./pages/NotFound"
+
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route 
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/register" element={<RegisterAndLogout />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
